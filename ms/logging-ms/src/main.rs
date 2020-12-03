e5b5498a0b54e4659fa11758c05cd203a75723a6
Alexei Ozerov <o3esound@yandex.ru>
2020-12-03 09:33:28 -0500

    Create boilerplate code for microservice, update Docker-related files

--- ms/logging-ms/src/main.rs
+++ ms/logging-ms/src/main.rs
@@ -1,3 +1,37 @@
+extern crate hyper;
+extern crate futures;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use hyper::server::{Request, Response, Service};
+
+use futures::future::Future;
+
+struct Microservice;
+
+impl Service for Microservice {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        info!("Microservice received a request: {:?}", request);
+        Box::new(futures::future::ok(Response::new()))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let address = "127.0.0.1:8080"
+        .parse()
+        .unwrap();
+    let server = hyper::server::Http::new()
+        .bind(&address, move || Ok(Microservice))
+        .unwrap();
+    
+    info!("Running microservice at {}", address);
+    server.run().unwrap();
 }
